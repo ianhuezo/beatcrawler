4d7c4216b41411afdb28c3516f22588f630d6d29
Ian Huezo <ianhuezo@live.com>
2021-01-29 13:59:19 -0800

    added readme and other resources for reference

--- beatsaber.py
+++ beatsaber.py
@@ -1,5 +1,10 @@
+import sys
+from beatcrawler.crawler import 
+
 if __name__ == "__main__":
-    pass
+    user_id = sys.argv[1]
+    page_number = sys.argv[2]
+    local_folder = sys.argv[3]
 
     # test_url = '5C870272E2159096D453F9420A8120980A5D4D29'
     # location = '/cdn/d771/5c870272e2159096d453f9420a8120980a5d4d29.zip'
